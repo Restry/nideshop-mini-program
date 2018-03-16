fe0195a9f1785d070614e25406418b5e70e6d3bf
592672849@qq.com <592672849@qq.com>
2018-03-16 18:58:30 +0800

    修改商品页面样式

--- config/api.js
+++ config/api.js
@@ -1,4 +1,5 @@
-const ApiRootUrl = 'http://127.0.0.1:8360/api/';
+const ApiRootUrl = 'http://192.168.1.107:8360/api/';
+//const ApiRootUrl = 'http://47.75.6.235:8360/api/';
 
 module.exports = {
   IndexUrl: ApiRootUrl + 'index/index', //首页数据接口
